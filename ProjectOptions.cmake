e92b5af08d837b587f2696f52cb5981792a550cb
Template Janitor <template.janitor@example.com>
2026-02-12 03:32:48 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(torii_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -58,110 +58,110 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(torii_setup_options)
+  option(torii_ENABLE_HARDENING "Enable hardening" ON)
+  option(torii_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    torii_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    torii_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  torii_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR torii_PACKAGING_MAINTAINER_MODE)
+    option(torii_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(torii_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(torii_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(torii_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(torii_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(torii_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(torii_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(torii_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(torii_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(torii_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(torii_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(torii_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(torii_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(torii_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(torii_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(torii_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(torii_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(torii_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(torii_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(torii_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(torii_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(torii_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(torii_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(torii_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      torii_ENABLE_IPO
+      torii_WARNINGS_AS_ERRORS
+      torii_ENABLE_SANITIZER_ADDRESS
+      torii_ENABLE_SANITIZER_LEAK
+      torii_ENABLE_SANITIZER_UNDEFINED
+      torii_ENABLE_SANITIZER_THREAD
+      torii_ENABLE_SANITIZER_MEMORY
+      torii_ENABLE_UNITY_BUILD
+      torii_ENABLE_CLANG_TIDY
+      torii_ENABLE_CPPCHECK
+      torii_ENABLE_COVERAGE
+      torii_ENABLE_PCH
+      torii_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  torii_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (torii_ENABLE_SANITIZER_ADDRESS OR torii_ENABLE_SANITIZER_THREAD OR torii_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(torii_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(torii_global_options)
+  if(torii_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    torii_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  torii_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(torii_ENABLE_HARDENING AND torii_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR torii_ENABLE_SANITIZER_UNDEFINED
+       OR torii_ENABLE_SANITIZER_ADDRESS
+       OR torii_ENABLE_SANITIZER_THREAD
+       OR torii_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${torii_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${torii_ENABLE_SANITIZER_UNDEFINED}")
+    torii_enable_hardening(torii_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(torii_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(torii_warnings INTERFACE)
+  add_library(torii_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  torii_set_project_warnings(
+    torii_warnings
+    ${torii_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -172,66 +172,66 @@ macro(myproject_local_options)
 
   if(NOT EMSCRIPTEN)
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    torii_enable_sanitizers(
+      torii_options
+      ${torii_ENABLE_SANITIZER_ADDRESS}
+      ${torii_ENABLE_SANITIZER_LEAK}
+      ${torii_ENABLE_SANITIZER_UNDEFINED}
+      ${torii_ENABLE_SANITIZER_THREAD}
+      ${torii_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(torii_options PROPERTIES UNITY_BUILD ${torii_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(torii_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      torii_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(torii_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    torii_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(torii_ENABLE_CLANG_TIDY)
+    torii_enable_clang_tidy(torii_options ${torii_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(torii_ENABLE_CPPCHECK)
+    torii_enable_cppcheck(${torii_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(torii_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    torii_enable_coverage(torii_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(torii_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(torii_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(torii_ENABLE_HARDENING AND NOT torii_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR torii_ENABLE_SANITIZER_UNDEFINED
+       OR torii_ENABLE_SANITIZER_ADDRESS
+       OR torii_ENABLE_SANITIZER_THREAD
+       OR torii_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    torii_enable_hardening(torii_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
